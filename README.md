52575de2a8fec9f28e1cf7d0f6989080ebb0badd
Niklas Rast <177048868+niklasrst@users.noreply.github.com>
2025-04-22 09:35:31 +0200

    feat(language-pack): added script to deploy languages to windows clients using a Intune package

--- README.md
+++ README.md
@@ -1,2 +1,50 @@
-# intune-windows-languagepack
-This repo contains a simple to use solution to deploy langauge packs online to Windows clients.
+# 🌐 Intune Windows Language Packs 🌐
+
+This repo contains a script to deploy a languagepack using `.cab` files to Windows 11 clients.
+The script will deploy the langauge using the `Add-WindowsPackage`-Command and then the client needs to reboot. The script will add a schedule task that will run at startup and change the langauge, this will affect in a second reboot before the user can sign-in so that the Display language will be changed completely. The schedule task will disable itself when running so that it only runs once at the next startup.
+
+## Customizations
+
+### Company Name
+Set the company name which will be used for the detection.
+``` powershell
+# Script settings
+$companyName = "YourCompanyNameHere"
+```
+
+## Prerequisites
+Download the current `mul_languages_and_optional_features_for_windows_11` ISO (available in MSDN), containing the `.cab` files for optional features and languages.
+Copy the following files for the language (in this example `en-us`) that you want to deploy into the package folder and create a `.INTUNEWIN` file with them and the `.ps1` script:
+- Microsoft-Windows-Client-Language-Pack_x64_`en-us`.cab
+- Microsoft-Windows-LanguageFeatures-Basic-`en-us`.cab
+- Microsoft-Windows-LanguageFeatures-Handwriting-`en-us`.cab
+- Microsoft-Windows-LanguageFeatures-OCR-`en-us`.cab
+- Microsoft-Windows-LanguageFeatures-Speech-`en-us`.cab
+- Microsoft-Windows-LanguageFeatures-TextToSpeech-`en-us`.cab
+
+## Deployment
+Create a `.INTUNEWIN` containing the `install-languagepack.ps1` and all `.cab`-files. Then create a new Intune App from it: 
+![intune-package](./assets/1.png)
+
+### Install
+Change the value of `-LanguageCode` to the [language code](https://docs.microsoft.com/en-us/windows-hardware/manufacture/desktop/available-language-packs-for-windows) that you want to install:
+``` powershell
+%WINDIR%\SysNative\WindowsPowerShell\v1.0\powershell.exe -NoLogo -NoProfile -NonInteractive -WindowStyle Hidden -ExecutionPolicy Bypass -File "install-languagepack.ps1" -install -LanguageCode "en-us"
+```
+
+### Uninstall
+Change the value of `-LanguageCode` to the language code that you want to uninstall:
+``` powershell
+%WINDIR%\SysNative\WindowsPowerShell\v1.0\powershell.exe -NoLogo -NoProfile -NonInteractive -WindowStyle Hidden -ExecutionPolicy Bypass -File "install-languagepack.ps1" -uninstall -LanguageCode "en-us"
+```
+<span style="color:cornflowerblue;font-weight:bold">🛈  HINT</span><br/>
+    You can select `Intune will forece a mandatory device restart` to directly force the device to reboot so that the language will be applied as the System language and everything will be switched to the language that you installed. A reboot is mandatory for the language to be applied. 
+
+![intune-package](./assets/2.png)
+
+### Detection
+Build the Intune detection on the Registry key: `HKLM\Software\$companyName\Windows-LanguagePack` and check the value `LanguageCode` to match the `-LanguageCode` that you selected for install
+
+![intune-package](./assets/3.png)
+
+Made with ❤️ by [Niklas Rast](https://github.com/niklasrst)
---
+++ install-languagepack.ps1
@@ -0,0 +1,142 @@
+﻿<#
+    .SYNOPSIS
+    Windows language pack installer
+
+    .DESCRIPTION
+    Install:   %WINDIR%\SysNative\WindowsPowerShell\v1.0\powershell.exe -NoLogo -NoProfile -NonInteractive -WindowStyle Hidden -ExecutionPolicy Bypass -File "install-languagepack.ps1" -install -LanguageCode "en-us"
+    Uninstall: %WINDIR%\SysNative\WindowsPowerShell\v1.0\powershell.exe -NoLogo -NoProfile -NonInteractive -WindowStyle Hidden -ExecutionPolicy Bypass -File "install-languagepack.ps1" -uninstall -LanguageCode "en-us"
+
+    .RUNSAS
+    SYSTEM
+
+    Language code information:
+    https://docs.microsoft.com/en-us/windows-hardware/manufacture/desktop/available-language-packs-for-windows
+
+    .ENVIRONMENT
+    PowerShell 5.0
+
+    .AUTHOR
+    Niklas Rast
+#>
+
+[CmdletBinding()]
+param(
+	[Parameter(Mandatory = $true, ParameterSetName = 'install')]
+	[switch]$install,
+	[Parameter(Mandatory = $true, ParameterSetName = 'uninstall')]
+	[switch]$uninstall,
+    [Parameter(Mandatory = $true)]
+    [string]$LanguageCode
+)
+
+# Script settings
+$companyName = ""
+$logFile = ('{0}\{1}.log' -f "C:\ProgramData\Microsoft\IntuneManagementExtension\Logs", [System.IO.Path]::GetFileNameWithoutExtension($MyInvocation.MyCommand.Name))
+
+if ($install) {
+    Start-Transcript -Path $logFile -Append
+    try {
+        # Install Language Pack
+        try {
+            # Install .cab files
+            $languagePackPath = Join-Path -Path $PsScriptRoot -ChildPath "*.cab"
+            $cabFiles = Get-ChildItem -Path $languagePackPath -File
+
+            if ($cabFiles.Count -eq 0) {
+                throw "No .cab files found in $PsScriptRoot."
+            }
+
+            foreach ($cabFile in $cabFiles) {
+                Write-Verbose "Installing language pack from $($cabFile.FullName)"
+                $process = Start-Process -FilePath "powershell.exe" -ArgumentList @(
+                    "-Command",
+                    "Add-WindowsPackage -PackagePath $($cabFile.FullName) -Online -NoRestart"
+                ) -NoNewWindow -Wait -PassThru
+                
+                if ($process.ExitCode -ne 0) {
+                    throw "Failed to install $($cabFile.FullName) with exit code $($process.ExitCode)."
+                }
+            }
+
+            # Create registry key and set value
+            $regPath = "HKLM:\Software\$companyName\Windows-LanguagePack"
+            if (-Not (Test-Path -Path $regPath)) {
+                New-Item -Path "HKLM:\Software\$companyName" -Name "Windows-LanguagePack" -Force | Out-Null
+            }
+            Set-ItemProperty -Path $regPath -Name "LanguageCode" -Value $LanguageCode -Force
+            Write-Verbose "Registry key created at $regPath with LanguagePack value set to $languagePackPath"
+        }
+        catch {
+            $tempErrorMessage = "`r`nCould not add language. Possible issue:`r`n{0}`r`nCancel installation.`r`n" -f $_.Exception.Message
+            Write-Error -Message $tempErrorMessage -Category WriteError -ErrorId 1
+            return 1
+        }
+        
+        # Set Display Language
+        try {
+            Write-Verbose "Setting System Language to $LanguageCode"
+
+            # Create task script
+            $scriptContent = @"
+param([string]`$LanguageCode, [string]`$TaskName)
+Start-Transcript -Path C:\ProgramData\Microsoft\IntuneManagementExtension\Logs\SetLanguageAndReboot.log -Append
+Set-SystemPreferredUILanguage -Language `$LanguageCode -Verbose
+Disable-ScheduledTask -TaskName `$TaskName
+Start-Sleep -Seconds 5
+Stop-Transcript
+Restart-Computer -Force
+"@
+
+            $scriptPath = "C:\Windows\Temp\SetLanguageAndReboot.ps1"
+            if (-Not (Test-Path -Path $scriptPath)) {
+                New-Item -Path $scriptPath -ItemType File -Force | Out-Null
+            }
+            Set-Content -Path $scriptPath -Value $scriptContent -Force
+
+            # Create a scheduled task to set the system preferred UI language and reboot
+            $taskName = "SetSystemLanguageAndReboot"
+            $taskAction = New-ScheduledTaskAction -Execute "powershell.exe" -Argument "-NoProfile -ExecutionPolicy Bypass -File `"$scriptPath`" -LanguageCode `"$LanguageCode`" -TaskName `"$taskName`""
+            $taskTrigger = New-ScheduledTaskTrigger -AtStartup
+            $taskPrincipal = New-ScheduledTaskPrincipal -UserId "SYSTEM" -LogonType ServiceAccount
+            $taskSettings = New-ScheduledTaskSettingsSet -AllowStartIfOnBatteries -DontStopIfGoingOnBatteries -StartWhenAvailable -Compatibility Win8
+
+            Register-ScheduledTask -TaskName $taskName -Action $taskAction -Trigger $taskTrigger -Principal $taskPrincipal -Settings $taskSettings
+            Write-Verbose "Scheduled task '$taskName' created to set system language and reboot."
+        }
+        catch {
+            $tempErrorMessage = "`r`nCould not configure language. Possible issue:`r`n{0}`r`nCancel installation.`r`n" -f $_.Exception.Message
+            Write-Error -Message $tempErrorMessage -Category WriteError -ErrorId 1
+            return 1
+        } 
+        
+        Write-Warning "Reboot required."
+        exit 1641
+    } catch {
+        $PSCmdlet.WriteError($_)
+        exit 1
+    }
+    Stop-Transcript
+}
+
+if ($uninstall) {
+    Start-Transcript -Path $logFile -Append
+    try {
+        # Uninstall Language Pack
+        try {
+            Write-Verbose "Uninstalling Language pack"
+            Uninstall-Language -Language $LanguageCode -Verbose
+        }
+        catch {
+            $tempErrorMessage = "`r`nCould not remove language. Possible issue:`r`n{0}`r`nCancel installation.`r`n" -f $_.Exception.Message
+            Write-Error -Message $tempErrorMessage -Category WriteError -ErrorId 1
+            return 1
+        }
+
+        Write-Warning "Reboot required."
+        exit 1641
+    } catch {
+        $PSCmdlet.WriteError($_)
+        exit 1
+    }
+    Stop-Transcript
+}
